274faaaee3f71484f82c324c5d626d84570fa7f0
Andrew Munsell <andrew@wizardapps.net>
2014-10-28 14:50:02 -0700

    Add confetti challenge

--- challenge-1/js/app.js
+++ challenge-1/js/app.js
@@ -5,34 +5,70 @@
 
 // After you have completed the basic core components, there are a few
 // optional features to work on:
-// -- If seconds > 60, timer display should switch to minute:second.
-// -- Include a warning when time is up (audio or visual).
 // -- Display time in miliseconds.
 
 
 // First, take a look at the index.html file to see the items you
 // will need to work with.
 
+var intervalId = null;
+
+var timeRemaining = null;
+var confettiSize = 10;
+
 // This function is the equivalent of document ready
 $(function() {
-	/* Hints:
-	 		-- Start by registering the click event for the button.
-	 		-- You will need to keep track of the number of second somewhere.
-	 		-- Remember javascript function can be assigned into variable.
-	 		-- Input values are strings, remember to parse it!
-			-- Regex for checking digit input is '^\\d+$'
-			-- Look up how setInterval() works, you will need it.
-	*/		
+	$('#countdownButton').click(function() {
+		var time = parseInt($('#time').val(), 10);
+		var confetti = parseInt($('#size').val(), 10);
+
+		if(isNaN(time)) {
+			return alert('You must input a number for the time.');
+		}
+
+		if(!isNaN(confetti)) {
+			confettiSize = confetti;
+		}
+
+		// Remove the old confetti
+		$('#confettiField').empty();
+
+		timeRemaining = time + 1;
+
+		countDown();
+		clearInterval(intervalId);
+		intervalId = setInterval(countDown, 1000);
+	});	
 });
 
 function countDown() {
-	// Hint: if time < 0, stop the countdown, otherwise refresh the timer display & call createConfetti().
-	// Hint: to stop countdown, look up clearInterval() function.
+	if(--timeRemaining < 1) {
+		clearInterval(intervalId);
+
+		$('#displayedTimer').text((timeRemaining % 60) + 's');
+		alert('TIME IS UP');
+	} else {
+		if(timeRemaining > 60) {
+			$('#displayedTimer').text(Math.floor(timeRemaining / 60) + 'm' + (timeRemaining % 60) + 's');
+		} else {
+			$('#displayedTimer').text((timeRemaining % 60) + 's');
+		}
+
+		createConfetti();
+	}
 }
 
 function createConfetti() {
 	// Get confetti's size. If user doesn't specify or type in invalid value, switch to a default size.
-
+	$('#confettiField')
+		.append(
+			$('<div class="confetti"></div>')
+				.css('width', confettiSize + 'px')
+				.css('height', confettiSize + 'px')
+				.css('top', Math.random() * window.innerHeight)
+				.css('left', Math.random() * window.innerWidth)
+				.css('background-color', 'rgb(' + [rand(0,255), rand(0,255), rand(0,255)].join(',') + ')')
+		);
 
 	//	Random color RGB, use Math.floor(Math.random() * 255)
 
@@ -51,4 +87,10 @@ function createConfetti() {
 	
 	// [Optional]
 	// Allow user to click on each confetti and remove it (1-2 line of code).
+}
+
+function rand(min, max) {
+	return Math.floor(
+		Math.random() * (max - min) + min
+	);
 }
